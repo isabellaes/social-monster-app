37f3d3e965ac58c8e2ea13f0a0fafd423ec84be8
isabellaes <borjessonisabella@gmail.com>
2024-11-23 14:53:29 +0100

    update monster avatar component

--- app/components/Monster.tsx
+++ app/components/Monster.tsx
@@ -1,34 +1,29 @@
-import { View, Image, Pressable, StyleSheet } from "react-native";
-import { Text } from "react-native-paper";
+import { View, Image, StyleSheet } from "react-native";
 import { Avatar } from "react-native-paper";
 import mapImages from "../utils/imageMapper";
 import { Monster } from "../utils/types";
 
 type MonsterAvatarProps = {
   monster: Monster;
-  onPressNavigation: () => void;
 };
 
 const MonsterAvatar = (props: MonsterAvatarProps) => {
   function getImageAvatar(avatar: string) {
     return (
-      <Image style={{ width: 100, height: 100 }} source={mapImages[avatar]} />
+      <Image style={{ width: 50, height: 50 }} source={mapImages[avatar]} />
     );
   }
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => props.onPressNavigation()}>
-        <Avatar.Image
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: props.monster.color,
-          }}
-          size={130}
-          source={() => getImageAvatar(props.monster.img)}
-        />
-        <Text style={styles.text}>{props.monster.name}</Text>
-      </Pressable>
+      <Avatar.Image
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: props.monster.color,
+        }}
+        size={60}
+        source={() => getImageAvatar(props.monster.img)}
+      />
     </View>
   );
 };
